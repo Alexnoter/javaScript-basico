3e2776fc327db80cd761bfba4e8908aed7928eb7
Alexander Velarde <alexandervelarde2511@gmail.com>
2023-05-13 05:37:03 -0400

    realizamos eventos sencillos en js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 //query tenemos que pasarle por parametros cual es el selector al que seleccionaremos
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 
